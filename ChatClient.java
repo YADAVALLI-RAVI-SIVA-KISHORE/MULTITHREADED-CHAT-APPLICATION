5da5d207100a9970c614af92732e066c442086de
YADAVALLI RAVI SIVA KISHORE <yrsk.kishore@gmail.com>
2025-09-30 20:50:41 +0530

    Update ChatClient.java

--- ChatClient.java
+++ ChatClient.java
@@ -1,5 +1,3 @@
-package com.Multithreaded_Chat_Application;
-
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
@@ -36,4 +34,5 @@ class ChatClient {
         clientSocket.close();
         inFromUser.close();
     }
+
 }
